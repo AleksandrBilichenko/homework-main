29d1c30b37620078e89411e5aa389d7084a94a53
PavelDevJS <jscommits@gmail.com>
2019-06-06 21:30:41 +0300

    Initial commit

---
+++ homework1/test.js
@@ -0,0 +1,9 @@
+const logBigNums = () => {
+    var array = [ 254, 115, 78,25, 91, 45, 37];
+    var maximaizer = 50;
+    for (var i = 0; i < array.length; i++)
+        if (array[i] >= maximaizer) 
+        alert(array[i])
+}
+
+logBigNums();
